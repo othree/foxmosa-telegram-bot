06045074235ba54a4534615200f591245f6eba3b
othree <othree@gmail.com>
2015-07-31 16:14:07 +0800

    Reorg telegram api response type

--- telegramapi/telegramapi.go
+++ telegramapi/telegramapi.go
@@ -18,6 +18,36 @@ type ProfileResult struct {
     Username string `json:"username"`
   } `json:"result"`
 }
+type Sticker struct {
+  Width int `json:"width"`
+  Height int `json:"height"`
+  Thumb struct {
+    FileID string `json:"file_id"`
+    FileSize int `json:"file_size"`
+    Width int `json:"width"`
+    Height int `json:"height"`
+  } `json:"thumb"`
+  FileID string `json:"file_id"`
+  FileSize int `json:"file_size"`
+}
+type Document struct {
+  FileName string `json:"file_name"`
+  MimeType string `json:"mime_type"`
+  Thumb struct {
+    FileID string `json:"file_id"`
+    FileSize int `json:"file_size"`
+    Width int `json:"width"`
+    Height int `json:"height"`
+  } `json:"thumb"`
+  FileID string `json:"file_id"`
+  FileSize int `json:"file_size"`
+}
+type Photo struct {
+  FileID string `json:"file_id"`
+  FileSize int `json:"file_size"`
+  Width int `json:"width"`
+  Height int `json:"height"`
+}
 type Update struct {
   UpdateID int `json:"update_id"`
   Message struct {
@@ -33,25 +63,10 @@ type Update struct {
       Title string `json:"title"`
     } `json:"chat"`
     Date int64 `json:"date"`
-    Document struct {
-      FileName string `json:"file_name"`
-      MimeType string `json:"mime_type"`
-      Thumb struct {
-        FileID string `json:"file_id"`
-        FileSize int `json:"file_size"`
-        Width int `json:"width"`
-        Height int `json:"height"`
-      } `json:"thumb"`
-      FileID string `json:"file_id"`
-      FileSize int `json:"file_size"`
-    } `json:"document"`
+    Sticker Sticker `json:"sticker"`
+    Document Document `json:"document"`
     Text string `json:"text"`
-    Photo []struct {
-      FileID string `json:"file_id"`
-      FileSize int `json:"file_size"`
-      Width int `json:"width"`
-      Height int `json:"height"`
-    } `json:"photo"`
+    Photo []Photo `json:"photo"`
     Caption string `json:"caption"`
   } `json:"message"`
 }
