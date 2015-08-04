d5b8c00ecbf7651f46c60259b13b589d0b94526a
othree <othree@gmail.com>
2015-08-04 15:20:28 +0800

    Add new config

--- foxmosa.go
+++ foxmosa.go
@@ -4,6 +4,7 @@ import (
   "./telegramapi"
   "./pierc"
   "github.com/vaughan0/go-ini"
+  "strconv"
 )
 
 func main() {
@@ -16,6 +17,19 @@ func main() {
   if !ok {
     panic("Telegram API token not available.")
   }
+  chat_id_str, ok := config.Get("telegram", "chat_id")
+  chat_id, err := strconv.Atoi(chat_id_str)
+  if !ok || err != nil {
+    panic("Telegram Chat ID not available.")
+  }
+  DB_HOST, ok := config.Get("pierc", "db_host")
+  if !ok || DB_HOST == "" {
+    DB_HOST = "127.0.0.1"
+  }
+  DB_PORT, ok := config.Get("pierc", "db_port")
+  if !ok || DB_PORT == "" {
+    DB_PORT = "3306"
+  }
   DB_USER, ok := config.Get("pierc", "db_user")
   if !ok {
     panic("Pierc db_user not set.")
@@ -23,10 +37,6 @@ func main() {
   DB_PASS, ok := config.Get("pierc", "db_pass")
   if !ok {
     panic("Pierc db_pass not set.")
-  }
-  DB_HOST, ok := config.Get("pierc", "db_host")
-  if !ok {
-    panic("Pierc db_host not set.")
   }
   DB_BASE, ok := config.Get("pierc", "db_base")
   if !ok {
@@ -41,11 +51,11 @@ func main() {
 
 
   messageChannel := make(chan *pierc.Message)
-  go pierc.Writer(DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_BASE, messageChannel)
+  go pierc.Writer(DB_USER + ":" + DB_PASS + "@tcp(" + DB_HOST + ":" + DB_PORT + ")/" + DB_BASE, messageChannel)
 
   go telegram_to_pierc(telegramapi.MakeUpdatesChannel(), messageChannel)
 
 
-  telegramapi.TrackingUpdates(token, offsetInit())
+  telegramapi.TrackingUpdates(token, chat_id, offsetInit())
 
 }
